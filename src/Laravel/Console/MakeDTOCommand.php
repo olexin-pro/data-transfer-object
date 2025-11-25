328c5175b35cca47b08d4c74a8984975bbbdbc50
ol3x1n <aizharolexin@yandex.kz>
2025-11-25 10:50:06 +0500

    feat: add make:dto command and refactor ConverterResolver
    - Adds a new make:dto Artisan command to easily generate Data Transfer Object classes from a customizable stub.
    - Refactors ConverterResolver to cache converter instances, improving performance by reusing objects instead of creating new ones on each call.
    - Bumps the required PHP version to ^8.3.

---
+++ src/Laravel/Console/MakeDTOCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Ol3x1n\DataTransferObject\Console\Commands;
+
+use http\Exception\RuntimeException;
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+
+class MakeDTOCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:dto
+        {name : Class name or path. Examples: UserDTO, Order/CreateOrderDTO, App\Integrations\PaymentDTO}
+        {--force : Overwrite file if it exists}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Data Transfer Object class';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(Filesystem $files): int
+    {
+        $input = trim((string) $this->argument('name'));
+        $input = str_replace('/', '\\', $input); // support both separators
+
+        // Base namespace for DTOs if full path is not provided
+        $baseNamespace = 'App\\DTO';
+
+        $isFqcn = Str::startsWith($input, 'App\\');
+
+        $fqcn = $isFqcn
+            ? $input
+            : trim($baseNamespace . '\\' . ltrim($input, '\\'), '\\');
+
+        $class = class_basename($fqcn);
+        $namespace = Str::beforeLast($fqcn, '\\') ?: $baseNamespace;
+
+        $relative = Str::after($fqcn, 'App\\');
+        $path = app_path(str_replace('\\', DIRECTORY_SEPARATOR, $relative) . '.php');
+
+        if ($files->exists($path) && ! $this->option('force')) {
+            $this->components->error("DTO already exists: {$path}");
+            return self::FAILURE;
+        }
+
+        $files->ensureDirectoryExists(dirname($path));
+
+        $stub = $this->resolveStub($files);
+
+        $contents = strtr($stub, [
+            '{{ namespace }}' => $namespace,
+            '{{ class }}'     => $class,
+        ]);
+
+        $files->put($path, $contents);
+
+        $this->components->info("DTO created successfully: {$fqcn}");
+        $this->components->twoColumnDetail('Path', $path);
+
+        return self::SUCCESS;
+    }
+
+    private function resolveStub(Filesystem $files): string
+    {
+        $published = base_path('stubs/dto.stub');
+        if ($files->exists($published)) {
+            return strval($files->get($published));
+        }else{
+            throw new RuntimeException('Stub file not found');
+        }
+    }
+}
