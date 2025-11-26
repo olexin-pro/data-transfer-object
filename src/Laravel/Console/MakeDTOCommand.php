7cfea7feacbb9fd2e9c581fa83067d8c17144c03
ol3x1n <aizharolexin@yandex.kz>
2025-11-26 12:16:56 +0500

    fix: add support for custom DTO stub generation with error handling
    - Implemented logic to load a custom DTO stub file and handle the case where the file does not exist, throwing a `FileNotFoundException`.
    - Introduced a default stub content method to provide a fallback structure for DTO classes.

--- src/Laravel/Console/MakeDTOCommand.php
+++ src/Laravel/Console/MakeDTOCommand.php
@@ -2,6 +2,7 @@
 
 namespace Ol3x1n\DataTransferObject\Laravel\Console;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use RuntimeException;
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
@@ -70,13 +71,40 @@ class MakeDTOCommand extends Command
         return self::SUCCESS;
     }
 
+    /**
+     * @throws FileNotFoundException
+     */
     private function resolveStub(Filesystem $files): string
     {
         $published = base_path('stubs/dto.stub');
         if ($files->exists($published)) {
             return strval($files->get($published));
-        }else{
-            throw new RuntimeException('Stub file not found');
         }
+
+        $packageStub = __DIR__ . '/../../../stubs/dto.stub';
+
+        if ($files->exists($packageStub)) {
+            return strval($files->get($packageStub));
+        }
+        return $this->defaultStubContent();
+    }
+
+    private function defaultStubContent(): string
+    {
+        return <<<'PHP'
+<?php
+
+namespace {{ namespace }};
+
+use Ol3x1n\DataTransferObject\AbstractDTO;
+use Ol3x1n\DataTransferObject\Field;
+use Ol3x1n\DataTransferObject\Enums\TypeEnum;
+
+class {{ class }} extends AbstractDTO
+{
+    // #[Field('name', TypeEnum::STRING)]
+    // public string $name;
+}
+PHP;
     }
 }
